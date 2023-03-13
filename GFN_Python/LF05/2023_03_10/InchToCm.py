b57adefdc182b720f97fd2d8354d89acaa008f3f
Snoocx <120705164+Snoocx@users.noreply.github.com>
2023-03-13 13:19:17 +0100

    added excercise

--- GFN_Python/LF05/2023_03_10/InchToCm.py
+++ GFN_Python/LF05/2023_03_10/InchToCm.py
@@ -1,12 +1,11 @@
-﻿#Schreiben Sie ein Programm, das den Anwender wie-
-#derholt dazu auffordert, einen Wert in Inch einzugeben. Der eingegebene Wert soll in Zentimeter umgerechnet und ausgegeben wer-
-#den. 
+﻿#Schreiben Sie ein Programm, das den Anwender wiederholt dazu auffordert, einen Wert in Inch einzugeben. 
+#Der eingegebene Wert soll in Zentimeter umgerechnet und ausgegeben werden. 
 #Das Programm soll nach der Eingabe des Werts 0 beendet werden.
 
 #Hinweis:
-#Bei einer while-Schleife wird immer angegeben, gemäß welcher Bedin-
-#gung wiederholt werden soll, und nicht, gemäß welcher Bedingung be-
-#endet werden soll. Daher müssen Sie in diesem Programm formulieren:
+#Bei einer while-Schleife wird immer angegeben, gemäß welcher Bedingung
+#wiederholt werden soll, und nicht, gemäß welcher Bedingung beendet werden soll.
+#Daher müssen Sie in diesem Programm formulieren:
 #Solange die Eingabe ungleich O ist.
 
 inches = 0.0
@@ -18,7 +17,7 @@ def GetInputAsInch():
    inches = float(inputInches) # Cast zu Float
    return inches
 
-# Für die Übersicht teile ich die Funktion auf.. es geht auch so:
+# Für die Übersicht teile ich die Zuweisung auf.. es geht auch so:
 # inches = float(input("Angabe in Inch:\n").replace(",", "."))
 
 def main():
---
+++ GFN_Python/LF05/2023_03_13/RechnungsbetragRabatt.py
@@ -0,0 +1,18 @@
+﻿def GetInputAsRevenue():
+   revenueString = input("Wie hoch war der Umsatz?\n")
+   revenueString = revenueString.replace(",", ".") 
+   return  float(revenueString) 
+
+def CalculateBillingAmountFromRevenue(revenue):
+    if revenue >= 500:
+        revenue = revenue * 0.9
+    elif revenue >= 100:
+        revenue = revenue * 0.95
+    return revenue
+
+def main():
+    revenue = GetInputAsRevenue()
+    billingAmount = CalculateBillingAmountFromRevenue(revenue)
+    print(f"Bei einem Umsatz von \"{str(revenue)} €\", ist der Rechnungsbetrag abzüglich des Rabattes: {str(billingAmount)} €")
+
+main()
