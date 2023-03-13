07a7bfc4f20f39d656b8393d41d7f6d35aa173bf
Snoocx <120705164+Snoocx@users.noreply.github.com>
2023-03-13 15:58:33 +0100

    added excercise

---
+++ GFN_Python/LF05/2023_03_13/GuessMyNumber.py
@@ -0,0 +1,24 @@
+import random
+
+def GetInputAsInt():
+    guessed = None
+    try:
+        guessed = int(input("Dein Rateversuch:\n"))
+    except:
+        guessed = int(input("Dein Rateversuch:\n"))
+    return guessed
+
+
+def GuessingLoop():
+    numberToGuess = random.randint(1,6)
+    countGuessed = 1
+
+    guessedNumber = GetInputAsInt()
+    while guessedNumber != numberToGuess:
+        countGuessed = countGuessed + 1
+        guessedNumber = GetInputAsInt()
+    return countGuessed
+
+countGuessed = GuessingLoop()
+
+print(f"Du hast beim {countGuessed}. Versuch die Zahl richtig erraten")
