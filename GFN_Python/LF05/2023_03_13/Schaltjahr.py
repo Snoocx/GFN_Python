28a1da110147e8dffdd558eb8db0607d52db981c
Snoocx <120705164+Snoocx@users.noreply.github.com>
2023-03-13 14:34:55 +0100

    added excercise

---
+++ GFN_Python/LF05/2023_03_13/Schaltjahr.py
@@ -0,0 +1,29 @@
+﻿# Ist die Jahreszahl durch vier teilbar, aber nicht durch 100, ist es ein Schaltjahr. 2008 fällt unter diese Regel.
+# Ist die Jahreszahl durch 100 teilbar, aber nicht durch 400, ist es kein Schaltjahr. 2100 wird kein Schaltjahr sein.
+# Ist die Jahreszahl durch 400 teilbar, dann ist es ein Schaltjahr. Deshalb war das Jahr 2000 ein Schaltjahr
+
+def GetInputAsYear():
+    return int(input("Bitte Jahr eingeben:\n"))
+
+def CheckIfYearIsLeapYear(year):
+    isLeapYear = False
+
+    if year % 4 == 0 and year % 100 != 0:
+        isLeapYear = True
+
+    if year % 100 == 0 and year % 400 != 0:
+        isLeapYear = False
+
+    if year % 400 == 0:
+        isLeapYear = True
+
+    return isLeapYear
+
+def main():
+    year = GetInputAsYear()                               
+    if CheckIfYearIsLeapYear(year):
+        print(f"{year} ist ein Schaltjahr") 
+    else:
+        print(f"{year} ist kein Schaltjahr") 
+
+main()
